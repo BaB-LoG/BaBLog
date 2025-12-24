dfe13b04de5a6a7bba4b7e6b8c25fbfdaf00d9aa
yshong1998 <yshong1998@gmail.com>
2025-12-24 20:30:07 +0900

    Squashed 'BaBLog-BE/' content from commit af591a5
    git-subtree-dir: BaBLog-BE
    git-subtree-split: af591a5465c15ade295831768c336499ff5623e8

---
+++ src/main/java/com/ssafy/bablog/BaBLogBeApplication.java
@@ -0,0 +1,23 @@
+package com.ssafy.bablog;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@MapperScan({
+        "com.ssafy.bablog.member.repository.mapper",
+        "com.ssafy.bablog.meal.repository.mapper",
+        "com.ssafy.bablog.meal_log.repository.mapper",
+        "com.ssafy.bablog.food.repository.mapper",
+        "com.ssafy.bablog.member_nutrient.repository.mapper",
+        "com.ssafy.bablog.report.repository.mapper"
+})
+@EnableScheduling
+public class BaBLogBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaBLogBeApplication.class, args);
+    }
+}
